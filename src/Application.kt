30ac5722f019d46134d98b3528d0eede674be6b5
岳人三牧 <gmimakiwithkamejiroandnana@gmail.com>
2020-05-10 14:56:11 +0900

    install jackson

--- src/Application.kt
+++ src/Application.kt
@@ -5,15 +5,24 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.response.*
 import io.ktor.request.*
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+
+data class Couple(
+    val id: Long
+)
 
 fun main(args: Array<String>) {
     val server = embeddedServer(Netty, 8080) {
         routing {
             get("/") {
                 call.respond(HttpStatusCode.OK, "Hello Ktor")
+            }
+            post("/couple") {
+                println("couple create")
+                call.respond(Couple(1))
             }
         }
     }
