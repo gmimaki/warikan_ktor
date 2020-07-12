9d16f5495b72aa9b8ec86a287a66079aa4dc42a8
岳人三牧 <gmimakiwithkamejiroandnana@gmail.com>
2020-07-12 17:38:12 +0900

    POSTリクエストが動いていないわけではなく、POSTリクエストの中の処理がうまくいかずレスポンスが返ってなかったっぽい

--- src/controller/CoupleController.kt
+++ src/controller/CoupleController.kt
@@ -1,11 +1,9 @@
 package com.example.controller
 
-import com.example.msg.CreateCoupleReq
 import com.example.msg.GetCoupleResMsg
 import com.example.service.CoupleService
 import io.ktor.application.call
 import io.ktor.http.HttpStatusCode
-import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.get
@@ -17,15 +15,26 @@ fun Route.coupleController() {
 
     route("/couple") {
         get {
+            print("GETTTTTTTTTTTTTTTTTTTTTTT")
             call.respond(
                 HttpStatusCode.OK,
                 coupleService.getAllCouples().map { couple -> GetCoupleResMsg(couple.name1, couple.name2, couple.ratio1, couple.ratio2) }
             )
         }
         post {
+            print("POSTTTTTTTTTTTTTTTTTTTTTTTTTTTTT")
+            /*
             val result = call.receive<CreateCoupleReq>()
-            coupleService.createCouple(result.name1, result.name2)
-            call.respond(HttpStatusCode.OK)
+            coupleService.createCouple(result.name1, result.name2, result.ratio1, result.ratio2)
+            call.respond(
+                HttpStatusCode.OK,
+                coupleService.getAllCouples().map { couple -> GetCoupleResMsg(couple.name1, couple.name2, couple.ratio1, couple.ratio2) }
+            )
+             */
+            call.respond(
+                HttpStatusCode.OK,
+                coupleService.getAllCouples().map { couple -> GetCoupleResMsg(couple.name1, couple.name2, couple.ratio1, couple.ratio2) }
+            )
         }
     }
 }
--- src/service/CoupleService.kt
+++ src/service/CoupleService.kt
@@ -12,11 +12,13 @@ class CoupleService {
         return result
     }
 
-    fun createCouple(name1: String, name2: String) {
+    fun createCouple(name1: String, name2: String, ratio1: Int, ratio2: Int) {
         transaction {
             Couple.new {
                 this.name1 = name1
                 this.name2 = name2
+                this.ratio1 = ratio1
+                this.ratio2 = ratio2
             }
         }
     }
