6c3bb6181e40f8c78d7e62ad71a60ea58438cb24
岳人三牧 <gmimakiwithkamejiroandnana@gmail.com>
2020-08-19 23:13:09 +0900

    デバッグ削除

--- src/controller/UserController.kt
+++ src/controller/UserController.kt
@@ -1,9 +1,8 @@
 package com.example.controller
 
 import com.example.msg.CreateUserReq
-import com.example.msg.GetCoupleResMsg
+import com.example.msg.CreateUserResMsg
 import com.example.msg.GetUserResMsg
-import com.example.service.CoupleService
 import com.example.service.UserAuthService
 import com.example.service.UserService
 import io.ktor.application.call
@@ -18,11 +17,9 @@ import kotlinx.coroutines.async
 
 fun Route.userController() {
     val userService = UserService()
-    val coupleService = CoupleService()
 
     route("/user") {
         get {
-            print("GETTTTTTTTTTTTTTTTTTTTTTT")
             call.respond(
                 HttpStatusCode.OK,
                 userService.getAllUsers().map { user -> GetUserResMsg(user.name, user.email, user.password) }
@@ -49,21 +46,16 @@ fun Route.userController() {
         post {
             val param = call.receive<CreateUserReq>()
             val user = userService.createUser(param.name, param.email, param.password)
-            print("THIS IS USER\n")
-            print(user.name + "\n")
-            print(user.email + "\n")
-            print(user.password + "\n")
             val token = async {
                 UserAuthService().createToken(user)
             }.await()
-            print("TOKKKKKKKKKKKKKKKKKKKKKEEEEEEEEEENNNNNNNNNNNNNNNNN\n")
-            print(token)
-            // TODO ちゃんと返ってるか確認 jacksonが効いてない?
+            // TODO なぜか以下の方式じゃないと返らない Accept: application/jsonをつけると何も返らない
+            /*
+                curl -X POST -H "Content-Type:application/json" http://localhost:8080/user/register -d '{"name":"","email":"", "password": ""}'
+             */
             call.respond(
                 HttpStatusCode.OK,
-                /*CreateUserResMsg(token)*/
- //               CreateUserResMsg("AOOOOOOOOOOOONNNNNNNNNNNNNn")
-                        coupleService.getAllCouples().map { couple -> GetCoupleResMsg(couple.name1, couple.name2, couple.ratio1, couple.ratio2) }
+                CreateUserResMsg(token)
             )
         }
     }
